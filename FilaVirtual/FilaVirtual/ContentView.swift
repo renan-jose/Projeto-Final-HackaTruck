c670a834c48a991aac07f794fef40fba9eef7227
Renan José <renan.jose2024@uel.br>
2025-10-06 14:13:20 -0300

    Add files via upload

--- FilaVirtual/FilaVirtual/ContentView.swift
+++ FilaVirtual/FilaVirtual/ContentView.swift
@@ -11,6 +11,32 @@ struct ContentView: View{
             
             VStack{
                 
+                Text("Selecione o tipo de usuário")
+                    .font(.system(size: 25))
+                    .foregroundColor(.white)
+                    
+                    
+                
+                Button("Cliente"){
+                    
+                }
+                .frame(width: 300, height: 60)
+                .background(Color.white)
+                .foregroundColor(.verde)
+                .font(.system(size: 30))
+                .cornerRadius(15)
+                .padding(.top, 8)
+                
+                
+                Button("Funcionário"){
+                    
+                }
+                .frame(width: 300, height: 60)
+                .background(Color.white)
+                .foregroundColor(.verde)
+                .font(.system(size: 30))
+                .cornerRadius(15)
+                .padding(.top, 8)
                 
                 
             }
